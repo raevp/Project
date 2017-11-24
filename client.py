ce3f417035f2b5bd0833cfb799e39b9a5b98fbe3
raevp <33897948+raevp@users.noreply.github.com>
2017-11-24 12:06:06 +0000

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 import sys  
 host = "" # connects to the host and the port of the server
-port = 12348 #
+port = 12345 #
 # create socket
 try:
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)# creat a TCP socket refer to IPv4 
@@ -24,10 +24,10 @@ try:
       print ('Send failed')
       sys.exit()
     # Receive data
-      print("Receive data from server")
-      data = s.recv(4096)# buffer size
-      data = data.decode("UTF-8")#decode and print data from the server
-      print (">>>" + data)
+    print("Receive data from server")
+    data = s.recv(4096)# buffer size
+    data = data.decode("UTF-8")#decode and print data from the server
+    print (">>>" + data)
     if messenge == "exit":
       print("Connection lost")
       s.close()
